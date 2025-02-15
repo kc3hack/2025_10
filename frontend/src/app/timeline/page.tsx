20dcb937b259fe3ef55bb9f76561dd018d30872f
RAIT-09 <rainbowtyourou.0963@gmail.com>
2025-02-15 15:42:56 +0900

    PostListコンポーネントの実装

--- frontend/src/app/timeline/page.tsx
+++ frontend/src/app/timeline/page.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
-import Post from '@/components/Post';
+import PostList from '@/components/PostList';
 
 export default function page() {
   // 投稿のサンプル
-  const post = {
-    id: 'example_post',
-    tanka: {
-      line1: 'KANSAI',
-      line2: '捉え難しき',
-      line3: 'お題かな',
-      line4: '古都の熱き歌',
-      line5: 'アプリで詠まん',
+  const posts = [
+    {
+      id: 'example_post',
+      tanka: {
+        line1: 'KANSAI',
+        line2: '捉え難しき',
+        line3: 'お題かな',
+        line4: '古都の熱き歌',
+        line5: 'アプリで詠まん',
+      },
+      original:
+        'K.A.N.S.A.Iというお題、どう捉えていいのか難しかった。京都や奈良は昔から短歌がアツいのかな。短歌を使ったアプリ作ろうかな。',
+      imageUrl: '',
+      date: new Date(),
+      user: {
+        id: 'example_user',
+        name: 'Name',
+        bio: 'bio',
+        iconUrl: '',
+      },
+      miyabi: 0,
     },
-    original:
-      'K.A.N.S.A.Iというお題、どう捉えていいのか難しかった。京都や奈良は昔から短歌がアツいのかな。短歌を使ったアプリ作ろうかな。',
-    imageUrl: '',
-    date: new Date(),
-    user: {
-      id: 'example_user',
-      name: 'Name',
-      bio: 'bio',
-      iconUrl: '',
+    {
+      id: 'example_post',
+      tanka: {
+        line1: 'KANSAI',
+        line2: '捉え難しき',
+        line3: 'お題かな',
+        line4: '古都の熱き歌',
+        line5: 'アプリで詠まん',
+      },
+      original:
+        'K.A.N.S.A.Iというお題、どう捉えていいのか難しかった。京都や奈良は昔から短歌がアツいのかな。短歌を使ったアプリ作ろうかな。',
+      imageUrl: '',
+      date: new Date(),
+      user: {
+        id: 'example_user',
+        name: 'Name',
+        bio: 'bio',
+        iconUrl: '',
+      },
+      miyabi: 0,
     },
-    miyabi: 0,
-  };
+  ];
 
   return (
     <div className='relative min-h-screen bg-white'>
       {/* 背景画像の設定 */}
       <div className="absolute inset-0 bg-[url('/bg.jpg')] bg-cover bg-center bg-fixed opacity-20" />
-      <Post post={post}></Post>
+      <PostList posts={posts}></PostList>
     </div>
   );
 }
---
+++ frontend/src/components/PostList.tsx
@@ -0,0 +1,29 @@
+// クライアントコンポーネント
+'use client';
+
+import { PostTypes } from '@/types/postTypes';
+import Post from '@/components/Post';
+
+// props の型定義
+type PostListProps = {
+  posts: PostTypes[];
+  className?: string;
+};
+
+/**
+ * 投稿のリストを表示するコンポーネント
+ * @component PostList
+ * @param {PostListProps} props - 投稿データの配列を含むオブジェクト
+ * @return {JSX.Elements} 投稿リスト表示するReactコンポーネント
+ */
+const PostList = ({ posts, className }: PostListProps) => {
+  return (
+    <div className='flex-1 overflow-auto'>
+      {posts.map((post, i) => (
+        <Post key={i} post={post} className={className} />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
