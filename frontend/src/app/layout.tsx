7a80cb6fea39203775724fbb67ccc892401686d0
RAIT-09 <rainbowtyourou.0963@gmail.com>
2025-02-15 14:14:46 +0900

    全体のフォントをNoto Serif JPに変更

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,25 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Noto_Serif_JP } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+const notoSerifJP = Noto_Serif_JP({
+  variable: '--font-noto-serif-jp',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Tankalizer",
-  description: "Tankalizer",
+  title: 'Tankalizer',
+  description: 'Tankalizer',
 };
 
 export default function RootLayout({
@@ -23,9 +28,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+    <html lang='ja'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSerifJP.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
