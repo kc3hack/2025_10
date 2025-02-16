42b9a0abd485ec552a59cf3e51636c0225f3cef0
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-17 03:25:36 +0900

    セッション情報によって表示コンポーネントを変える

--- frontend/src/app/yomu/page.tsx
+++ frontend/src/app/yomu/page.tsx
@@ -6,13 +6,44 @@ import Image from 'next/image';
 import styles from './page.module.scss';
 import { VscClose } from 'react-icons/vsc';
 import Dialog from '@/components/Dialog';
+import { useSession, signIn } from 'next-auth/react';
+import Link from 'next/link';
 
 const MAX_LENGTH = 140; // 最大文字数
 const MIN_LENGTH = 40; // 最小文字数→短歌にいい感じに変換するにはこれくらい必要
 
-const Page = () => {
+const Page = (): React.ReactNode => {
+  const session = useSession();
+
+  return (
+    <div className='relative min-h-screen'>
+      {/* 背景 */}
+      <div
+        className={`${styles.no_drag} ${styles.no_select} fixed left-0 top-0 size-full bg-black/60`}
+      >
+        <Image src='/bg.jpg' alt='background' fill className='object-cover opacity-20' />
+      </div>
+
+      {session.status === 'loading' ? (
+        <p>loading...</p>
+      ) : session.status === 'authenticated' ? (
+        // ログインしている時はSignedInPageを表示
+        <SignedInPage />
+      ) : (
+        // ログインしていない時はNotSignedInPageを表示
+        <NotSignedInPage />
+      )}
+    </div>
+  );
+};
+
+/**
+ * ログインしている時に表示されるページ
+ */
+const SignedInPage = (): React.ReactNode => {
   const [text, setText] = useState('');
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const session = useSession();
 
   const router = useRouter();
 
@@ -25,75 +56,91 @@ const Page = () => {
   };
 
   return (
-    <div className='relative min-h-screen'>
-      {/* 背景 */}
-      <div
-        className={`${styles.no_drag} ${styles.no_select} fixed left-0 top-0 size-full bg-black/60`}
-      >
-        <Image src='/bg.jpg' alt='background' fill className='object-cover opacity-20' />
-      </div>
-
-      <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full'>
-        <div className='max-w-[40rem] h-[30rem] bg-white rounded-lg shadow-lg p-8 mx-auto w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2'>
-          <div
-            className='block size-8 hover:opacity-70 mb-4 cursor-pointer'
-            onClick={() => {
-              if (text.length > 0) {
-                // 下書きがあるときは警告を出す
-                setIsDialogOpen(true);
-              } else {
-                // 無いときはそのままタイムラインに戻る
-                router.push('/timeline');
-              }
-            }}
-          >
-            <VscClose className='size-full' />
-          </div>
-          <div className='flex items-center gap-4'>
-            <Image
-              // src={post.user.iconUrl !== '' ? post.user.iconUrl : '/iconDefault.png'}
-              src='/iconDefault.png'
-              height={50}
-              width={50}
-              alt='Icon'
-              className='rounded-full -mt-5'
-            />
-            <textarea
-              className='w-full resize-none overflow-hidden p-3 outline-none md:text-lg'
-              placeholder='いま何してんの？'
-              onChange={(e) => {
-                resizeTextArea(e);
-              }}
-            ></textarea>
-          </div>
-          <button
-            className={`absolute bottom-10 left-1/2 -translate-x-1/2 z-30 w-16 h-16 border-2 border-transparent shadow-lg bg-orange-400 hover:bg-orange-500 text-4xl font-bold text-white font-shinryu rounded-full`}
-          >
-            詠
-          </button>
-          <p
-            className={
-              text.length < MIN_LENGTH || text.length > MAX_LENGTH
-                ? 'text-red-500'
-                : 'text-green-500'
+    <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full'>
+      <div className='max-w-[40rem] h-[30rem] bg-white rounded-lg shadow-lg p-8 mx-auto w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2'>
+        <div
+          className='block size-8 hover:opacity-70 mb-4 cursor-pointer'
+          onClick={() => {
+            if (text.length > 0) {
+              // 下書きがあるときは警告を出す
+              setIsDialogOpen(true);
+            } else {
+              // 無いときはそのままタイムラインに戻る
+              router.push('/timeline');
             }
-          >
-            {text.length}
-          </p>
-          <Dialog
-            isOpen={isDialogOpen}
-            description='ページを離れると下書きは保存されません。よろしいですか？'
-            isOnlyOK={false}
-            yesCallback={() => {
-              setIsDialogOpen(false);
-              router.push('/timeline'); // 「はい」を押したらタイムラインに戻る
-            }}
-            noCallback={() => {
-              setIsDialogOpen(false);
-            }}
-            yesText='OK'
-            noText='キャンセル'
+          }}
+        >
+          <VscClose className='size-full' />
+        </div>
+        <div className='flex items-center gap-4'>
+          <Image
+            src={session.data?.user?.image ?? ''}
+            height={50}
+            width={50}
+            alt='Icon'
+            className='rounded-full -mt-5'
           />
+          <textarea
+            className='w-full resize-none overflow-hidden p-3 outline-none md:text-lg'
+            placeholder='いま何してんの？'
+            onChange={(e) => {
+              resizeTextArea(e);
+            }}
+          ></textarea>
+        </div>
+        <button
+          className={`absolute bottom-10 left-1/2 -translate-x-1/2 z-30 w-16 h-16 border-2 border-transparent shadow-lg bg-orange-400 hover:bg-orange-500 text-4xl font-bold text-white font-shinryu rounded-full`}
+        >
+          詠
+        </button>
+        <p
+          className={
+            text.length < MIN_LENGTH || text.length > MAX_LENGTH ? 'text-red-500' : 'text-green-500'
+          }
+        >
+          {text.length}
+        </p>
+        <Dialog
+          isOpen={isDialogOpen}
+          description='ページを離れると下書きは保存されません。よろしいですか？'
+          isOnlyOK={false}
+          yesCallback={() => {
+            setIsDialogOpen(false);
+            router.push('/timeline'); // 「はい」を押したらタイムラインに戻る
+          }}
+          noCallback={() => {
+            setIsDialogOpen(false);
+          }}
+          yesText='OK'
+          noText='キャンセル'
+        />
+      </div>
+    </div>
+  );
+};
+
+/**
+ * ログインしていない時に表示されるページ
+ */
+const NotSignedInPage = (): React.ReactNode => {
+  return (
+    <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full'>
+      <div className='max-w-[40rem] h-[30rem] bg-white rounded-lg shadow-lg p-8 mx-auto w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2'>
+        <Link
+          href='/timeline'
+          className='block size-8 hover:opacity-70 mb-4 cursor-pointer'
+          onClick={() => {}}
+        >
+          <VscClose className='size-full' />
+        </Link>
+        <div className='flex flex-col items-center justify-center'>
+          <p className='mb-4'>投稿するにはログインしてください</p>
+          <button
+            onClick={() => signIn()}
+            className='bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded'
+          >
+            ログイン
+          </button>
         </div>
       </div>
     </div>
