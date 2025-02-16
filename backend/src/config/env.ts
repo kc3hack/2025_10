8da935ea6940f26cc832f939891fbf5ec6f4d986
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-16 15:22:40 +0900

    env追加

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,6 +7,9 @@ dotenv.config();
 const envSchema = z.object({
   FRONTEND_URL: z.string().url(),
   PORT: z.coerce.number().optional(),
+  S3_ACCESS_KEY_ID: z.string(),
+  S3_SECRET_ACCESS_KEY: z.string(),
+  S3_BUCKET_NAME: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
