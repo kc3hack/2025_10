d74fb1b9f8925258963c1a05820bf0db03cbaac4
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-17 00:22:06 +0900

    投稿ページUIだけ

---
+++ frontend/src/app/yomu/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from './page.module.scss';
+import { VscClose } from 'react-icons/vsc';
+import Link from 'next/link';
+
+const Page = () => {
+  const [text, setText] = useState('');
+
+  // テキストエリアの高さを自動で調整する
+  const resizeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    e.target.style.height = 'auto';
+    e.target.style.height = `${e.target.scrollHeight}px`;
+
+    setText(e.target.value);
+  };
+
+  return (
+    <div className='relative min-h-screen'>
+      {/* 背景 */}
+      <div
+        className={`${styles.no_drag} ${styles.no_select} fixed left-0 top-0 size-full bg-black/60`}
+      >
+        <Image src='/bg.jpg' alt='background' fill className='object-cover opacity-20' />
+      </div>
+
+      <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full'>
+        <div className='max-w-[40rem] h-[30rem] bg-white rounded-lg shadow-lg p-8 mx-auto w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2'>
+          <Link href='/timeline' className='block size-8 hover:opacity-70 mb-4'>
+            <VscClose className='size-full' />
+          </Link>
+          <div className='flex items-center gap-4'>
+            <Image
+              // src={post.user.iconUrl !== '' ? post.user.iconUrl : '/iconDefault.png'}
+              src='/iconDefault.png'
+              height={50}
+              width={50}
+              alt='Icon'
+              className='rounded-full -mt-5'
+            />
+            <textarea
+              className='w-full resize-none overflow-hidden p-3 outline-none md:text-lg'
+              placeholder='いま何してんの？'
+              onChange={(e) => {
+                resizeTextArea(e);
+              }}
+            ></textarea>
+          </div>
+          <button
+            className={`absolute bottom-10 left-1/2 -translate-x-1/2 z-30 w-16 h-16 border-2 border-transparent shadow-lg bg-orange-400 hover:bg-orange-500 text-4xl font-bold text-white font-shinryu rounded-full`}
+          >
+            詠
+          </button>
+          <p className={text.length < 40 || text.length > 140 ? 'text-red-500' : 'text-green-500'}>
+            {text.length}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
