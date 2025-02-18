42e018f702e8558c50966403b58cac234cdbf844
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-18 13:55:59 +0900

    コメントアウト追加、最大画像サイズ定義

--- frontend/src/app/yomu/page.tsx
+++ frontend/src/app/yomu/page.tsx
@@ -17,6 +17,7 @@ import FileUploadButton from '@/components/FileUploadButton';
 
 const MAX_LENGTH = 140; // 最大文字数
 const MIN_LENGTH = 40; // 最小文字数→短歌にいい感じに変換するにはこれくらい必要
+const MAX_IMAGE_SIZE = 5; // 最大画像サイズ
 
 enum PostStatus {
   INITIAL = 'initial',
@@ -38,7 +39,7 @@ const Page = (): React.ReactNode => {
       </div>
 
       {session.status === 'loading' ? (
-        <p>loading...</p>
+        <p className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>loading...</p>
       ) : session.status === 'authenticated' ? (
         // ログインしている時はSignedInPageを表示
         <SignedInPage />
@@ -86,7 +87,7 @@ const SignedInPage = (): React.ReactNode => {
     setIsDialogOpen(false);
   }, []);
 
-  // 画像がインポートされた時の処理
+  // ドラッグアンドドロップで画像をアップロードする
   const onDrop = (e: React.DragEvent<HTMLTextAreaElement>): void => {
     e.preventDefault();
     if (e.dataTransfer.files.length !== 1) {
@@ -99,6 +100,7 @@ const SignedInPage = (): React.ReactNode => {
     uploadFile(file);
   };
 
+  // 画像アップロードボタン
   const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) {
@@ -107,6 +109,7 @@ const SignedInPage = (): React.ReactNode => {
     uploadFile(file);
   };
 
+  // 画像がインポートされた時の処理
   const uploadFile = (file: File) => {
     if (!judgeImage(file)) {
       console.log('画像ファイルをアップロードしてください');
@@ -114,9 +117,9 @@ const SignedInPage = (): React.ReactNode => {
       return;
     }
 
-    if (calcFileSize(file) > 5) {
-      console.log('ファイルサイズが5MBを超えています');
-      alert('ファイルサイズが5MBを超えています');
+    if (calcFileSize(file) > MAX_IMAGE_SIZE) {
+      console.log(`ファイルサイズが${MAX_IMAGE_SIZE}MBを超えています`);
+      alert(`ファイルサイズが${MAX_IMAGE_SIZE}MBを超えています`);
       return;
     }
 
@@ -152,98 +155,110 @@ const SignedInPage = (): React.ReactNode => {
   return (
     <>
       <main className='fixed left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2'>
-        <div
-          className={`mx-auto min-h-[30rem] w-11/12 max-w-[40rem] rounded-lg bg-white p-8 shadow-lg md:w-3/4 lg:w-2/3 xl:w-1/2`}
-        >
+        {/* 投稿フォーム */}
+        {postStatus !== PostStatus.SUCCESS && (
           <div
-            className='mb-4 block size-8 cursor-pointer hover:opacity-70'
-            onClick={() => {
-              if (text.length > 0 || file) {
-                // 下書きがあるときは警告を出す
-                setIsDialogOpen(true);
-              } else {
-                // 無いときはそのままタイムラインに戻る
-                router.push('/timeline');
-              }
-            }}
+            className={`mx-auto min-h-[30rem] w-11/12 max-w-[40rem] rounded-lg bg-white p-8 shadow-lg md:w-3/4 lg:w-2/3 xl:w-1/2`}
           >
-            <VscClose className='size-full' />
-          </div>
-          <div className='gap-4 md:flex md:items-start'>
-            <Image
-              src={session.data?.user?.image ?? ''}
-              height={50}
-              width={50}
-              alt='Icon'
-              className='rounded-full'
-            />
-            <textarea
-              className={`w-full resize-none rounded-lg border-2 border-dotted p-3 outline-none md:text-lg ${
-                isDragActive ? 'border-red-400' : 'border-gray-300/0'
-              } ${file ? 'min-h-40' : 'min-h-80'}`}
-              placeholder='いま何してんの？'
-              onChange={(e) => {
-                onChangeTextArea(e);
+            {/* 閉じるボタン */}
+            <div
+              className='mb-4 block size-8 cursor-pointer hover:opacity-70'
+              onClick={() => {
+                if (text.length > 0 || file) {
+                  // 下書きがあるときは警告を出す
+                  setIsDialogOpen(true);
+                } else {
+                  // 無いときはそのままタイムラインに戻る
+                  router.push('/timeline');
+                }
               }}
-              onDrop={(e) => {
-                onDrop(e);
-                setIsDragActive(false);
-              }}
-              onDragEnter={() => {
-                setIsDragActive(true);
-              }}
-              onDragLeave={() => {
-                setIsDragActive(false);
-              }}
-            ></textarea>
-          </div>
-          {file && (
-            <div className='relative mx-auto mb-4 aspect-square w-1/2'>
-              <div className='absolute right-1 top-1 z-10 rounded-lg bg-white/40 hover:bg-white/70'>
-                <VscClose
-                  className='size-8 cursor-pointer hover:opacity-70'
-                  onClick={() => {
-                    setFile(null);
-                  }}
-                />
-              </div>
-              <Image src={file.filePath} alt='upload' fill className='rounded object-contain' />
-              <p className='absolute bottom-0 left-0'>{file.file.name}</p>
+            >
+              <VscClose className='size-full' />
             </div>
-          )}
-          <p className={`${canPost ? 'text-green-500' : 'text-red-500'} text-right`}>
-            {text.length}文字
-          </p>
-          <button
-            className={`mx-auto block size-16 rounded-full border-2 border-transparent bg-orange-400 font-shinryu text-4xl font-bold text-white shadow-lg transition-transform duration-300  ${
-              canPost ? 'hover:scale-105 hover:bg-orange-500 active:scale-95' : 'opacity-50'
-            }`}
-            disabled={!canPost}
-            onClick={onClickYomuButton}
-          >
-            詠
-          </button>
 
-          {!canPost && (
-            <p className='mt-2 text-center text-sm text-red-500'>
-              ※{MIN_LENGTH}文字以上{MAX_LENGTH}文字以内で投稿できます。
+            {/* ユーザーアイコンとテキストエリア */}
+            <div className='gap-4 md:flex md:items-start'>
+              <Image
+                src={session.data?.user?.image ?? ''}
+                height={50}
+                width={50}
+                alt='Icon'
+                className='rounded-full'
+              />
+              <textarea
+                className={`w-full resize-none rounded-lg border-2 border-dotted p-3 outline-none md:text-lg ${
+                  isDragActive ? 'border-red-400' : 'border-gray-300/0'
+                } ${file ? 'min-h-40' : 'min-h-80'}`}
+                placeholder='いま何してんの？'
+                onChange={(e) => {
+                  onChangeTextArea(e);
+                }}
+                onDrop={(e) => {
+                  onDrop(e);
+                  setIsDragActive(false);
+                }}
+                onDragEnter={() => {
+                  setIsDragActive(true);
+                }}
+                onDragLeave={() => {
+                  setIsDragActive(false);
+                }}
+              ></textarea>
+            </div>
+
+            {/* 画像プレビュー */}
+            {file && (
+              <div className='relative mx-auto mb-4 aspect-square w-1/2'>
+                <div className='absolute right-1 top-1 z-10 rounded-lg bg-white/40 hover:bg-white/70'>
+                  <VscClose
+                    className='size-8 cursor-pointer hover:opacity-70'
+                    onClick={() => {
+                      setFile(null);
+                    }}
+                  />
+                </div>
+                <Image src={file.filePath} alt='upload' fill className='rounded object-contain' />
+                <p className='absolute bottom-0 left-0'>{file.file.name}</p>
+              </div>
+            )}
+
+            {/* 文字数表示 */}
+            <p className={`${canPost ? 'text-green-500' : 'text-red-500'} text-right`}>
+              {text.length}文字
             </p>
-          )}
 
-          {/* 画像アップロードボタン */}
-          <FileUploadButton onChangeFileCallback={onChangeFile} />
+            {/* 投稿ボタン */}
+            <button
+              className={`mx-auto block size-16 rounded-full border-2 border-transparent bg-orange-400 font-shinryu text-4xl font-bold text-white shadow-lg transition-transform duration-300  ${
+                canPost ? 'hover:scale-105 hover:bg-orange-500 active:scale-95' : 'opacity-50'
+              }`}
+              disabled={!canPost}
+              onClick={onClickYomuButton}
+            >
+              詠
+            </button>
 
-          {/* 下書き削除のダイアログ */}
-          <Dialog
-            isOpen={isDialogOpen}
-            description='ページを離れると下書きは保存されません。よろしいですか？'
-            isOnlyOK={false}
-            yesCallback={yesCallback}
-            noCallback={noCallback}
-            yesText='OK'
-            noText='キャンセル'
-          />
-        </div>
+            {!canPost && (
+              <p className='mt-2 text-center text-sm text-red-500'>
+                ※{MIN_LENGTH}文字以上{MAX_LENGTH}文字以内で投稿できます。
+              </p>
+            )}
+
+            {/* 画像アップロードボタン */}
+            <FileUploadButton onChangeFileCallback={onChangeFile} />
+
+            {/* 下書き削除のダイアログ */}
+            <Dialog
+              isOpen={isDialogOpen}
+              description='ページを離れると下書きは保存されません。よろしいですか？'
+              isOnlyOK={false}
+              yesCallback={yesCallback}
+              noCallback={noCallback}
+              yesText='OK'
+              noText='キャンセル'
+            />
+          </div>
+        )}
       </main>
 
       {/* 投稿中ローディング */}
@@ -253,6 +268,7 @@ const SignedInPage = (): React.ReactNode => {
         </div>
       )}
 
+      {/* 投稿成功時の表示 短歌を表示するアニメーション再生 */}
       {postStatus === PostStatus.SUCCESS && (
         <AfterYomu
           tanka={resResult?.tanka ?? []}
