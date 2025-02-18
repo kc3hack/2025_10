1461b945dfb59a798e008bdc4c74b5012b80e7cf
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-18 16:02:01 +0900

    dockerとlocalでバックエンドURLの切り替え

--- frontend/src/app/sample/page.tsx
+++ frontend/src/app/sample/page.tsx
@@ -10,7 +10,7 @@ interface SampleData {
   password: string;
 }
 
-const client = hc<AppType>('http://backend:8080');
+const client = hc<AppType>(process.env.BACKEND_URL ?? 'http://localhost:8080');
 
 // サンプルデータを取得する関数
 const postSampleData = async (): Promise<SampleData | null> => {
