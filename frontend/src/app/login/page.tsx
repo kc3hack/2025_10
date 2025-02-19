5817b51e85d9c69dc9e1aabf898ec1864577a7d4
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-19 23:14:29 +0900

    fix dynamic api

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { signIn, providerMap } from '@/auth/config';
 import { AuthError } from 'next-auth';
 
 export default async function SignInPage(props: {
-  searchParams: { callbackUrl: string | undefined };
+  searchParams: Promise<{ callbackUrl: string | undefined }>;
 }) {
   return (
     <div className='flex flex-col gap-2'>
@@ -14,7 +14,7 @@ export default async function SignInPage(props: {
             'use server';
             try {
               await signIn(provider.id, {
-                redirectTo: props.searchParams?.callbackUrl ?? '',
+                redirectTo: (await props.searchParams)?.callbackUrl ?? '',
               });
             } catch (error) {
               if (error instanceof AuthError) {
