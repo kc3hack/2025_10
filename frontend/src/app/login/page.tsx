a4fa9da7e80834e95800aa8223fb20331bd04edb
natanata25 <136555690+natanata25@users.noreply.github.com>
2025-02-20 17:49:41 +0900

    plofileのフォント変更

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -13,20 +13,17 @@ export default async function SignInPage({ searchParams }: Props) {
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-gray-100 to-gray-300'>
-      {/* ヘッダ /}
+      {/* ヘッダ */}
       <div className='fixed top-0 z-40 flex h-12 w-full items-center justify-center bg-white font-kokuryu text-2xl'>
         <div className=''>Tankalizer</div>
       </div>
 
-      {/ 背景画像 */}
       <div className='fixed left-0 top-0 z-[-1] h-screen w-full opacity-20'>
-        <Image src='/bg.jpg' layout='fill' objectFit='cover' alt='Background'></Image>
+        <Image src='/bg.jpg' alt='Background' fill className='object-cover' />
       </div>
 
       <div className='w-full max-w-md space-y-6 rounded-xl border border-gray-300 bg-white p-6 shadow-lg'>
-        <h1 className='border-b border-gray-300 pb-4 text-center text-2xl font-bold text-gray-800'>
-          共に歌を詠まん
-        </h1>
+        <h1 className='text-center font-shinryu text-4xl'>共に歌を詠まん</h1>
         {Object.values(providerMap).map((provider) => (
           <form
             key={provider.id}
