a39989468c5545a1b92b111351813df09f172058
RAIT-09 <rainbowtyourou.0963@gmail.com>
2025-02-21 17:05:56 +0900

    雅ランキング上部UI

--- frontend/src/app/(main)/ranking/page.tsx
+++ frontend/src/app/(main)/ranking/page.tsx
@@ -6,10 +6,18 @@ import Timeline from '@/components/Timeline';
 const MAX = 10; // タイムラインに表示できる最大投稿数
 
 const Page = () => {
+  const dateEnd = new Date();
+  const dateStart = new Date(dateEnd.getTime());
+  dateStart.setDate(dateStart.getDate() - 7);
   return (
     <div className='relative min-h-screen'>
       {/* タイムライン */}
-      <div className='mx-auto max-w-lg'>
+      <div className='mx-auto max-w-sm pt-5 lg:max-w-lg'>
+        <div className='mx-4 rounded-xl border-2 border-gray-300 bg-gradient-to-r from-amber-100 to-amber-50 py-2 text-center shadow-lg'>
+          <a className='text-lg font-semibold text-gray-700'>雅ランキング</a>
+          <div className='my-1 border-b' />
+          <p>{`${dateStart.toLocaleDateString()} ～ ${dateEnd.toLocaleDateString()}`}</p>
+        </div>
         <Timeline limit={MAX} max={MAX} mode='ranking' />
       </div>
     </div>
