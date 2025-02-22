a63ee3d622891743ea776ac58ffae88c32144a97
ABfry <is0605ek@ed.ritsumei.ac.jp>
2025-02-23 00:24:21 +0900

    news投稿

--- backend/src/lib/postNews.ts
+++ backend/src/lib/postNews.ts
@@ -2,6 +2,7 @@ import type { Context } from 'hono';
 import { env } from '../config/env.js';
 import getNews from './getNews.js';
 import generateTanka from './gemini.js';
+import createPostHandler from '../controllers/Post/createPostHandler.js';
 
 const printLine = (): void => {
   console.log('--------------------------------');
@@ -42,20 +43,54 @@ const postNews = async (requestApiKey: string) => {
   // 投稿の原文
   const originalText = `${news.title}\n${news.description}\n${news.url}`;
 
-  // そうたさんが作った関数呼ぶ？　原文、画像（不要）、newsユーザーのアイコン画像リンク、ユーザー名（news）
-  // const postResponse = createPostHandler(originalText);
+  console.log('originalText', originalText);
 
-  const postResponse = {
-    isSuccess: true,
-    tanka: {
-      line0: 'ニュースです',
-      line1: 'これはニュースよ',
-      line2: 'ニュースです',
-      line3: 'リアルタイムの',
-      line4: 'ニュースですわよ',
-    },
-  };
-  return postResponse;
+  try {
+    const formData = new FormData();
+    formData.append('original', originalText);
+    formData.append('user_icon', 'https://avatars.githubusercontent.com/u/200390078?v=4');
+    formData.append('user_name', 'NEWS');
+
+    // そうたさんが作った関数呼ぶ？　原文、画像（不要）、newsユーザーのアイコン画像リンク、ユーザー名（news）
+    const postResponse = await fetch(`http://localhost:8080/post`, {
+      method: 'POST',
+      body: formData,
+    });
+
+    console.log('postResponse', postResponse);
+
+    if (!postResponse.ok) {
+      return {
+        isSuccess: false,
+        tanka: {
+          line0: '',
+          line1: '',
+          line2: '',
+          line3: '',
+          line4: '',
+        },
+      };
+    }
+
+    const json = await postResponse.json();
+
+    return {
+      isSuccess: true,
+      tanka: json.tanka,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      isSuccess: false,
+      tanka: {
+        line0: '投稿に失敗しました',
+        line1: '',
+        line2: '',
+        line3: '',
+        line4: '',
+      },
+    };
+  }
 };
 
 export default postNews;
