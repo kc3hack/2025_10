276ab719c00ca7aa272d1f53d80edb44bd3c46da
Yone <is0698pi@ed.ritsumei.ac.jp>
2025-02-23 01:41:21 +0900

    スキーマ例修正

--- backend/src/schema/Sample/sampleNewsTankaSchema.ts
+++ backend/src/schema/Sample/sampleNewsTankaSchema.ts
@@ -2,7 +2,7 @@ import { z } from '@hono/zod-openapi';
 
 export const sampleNewsTankaSchema = z.object({
   apiKey: z.string().openapi({
-    example: 'abc123...',
+    example: 'abc123',
     description: 'APIキー',
   }),
 });
@@ -22,11 +22,11 @@ export const sampleNewsTankaResponseSchema = z.object({
     })
     .openapi({
       example: {
-        line0: 'ニュースです',
-        line1: 'これはニュースよ',
-        line2: 'ニュースです',
-        line3: 'リアルタイムの',
-        line4: 'ニュースですわよ',
+        line0: '巷説の',
+        line1: '風の便りと',
+        line2: '聞きなして',
+        line3: '都の噂を',
+        line4: '語り継がんか',
       },
       description: 'ニュースを取得し、短歌にした結果',
     }),
